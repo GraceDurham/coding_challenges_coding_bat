549ee80cd5732a949be4bee128cb280fcb45ccf0
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2019-03-31 15:38:18 -0500

    refactor code to one line

--- missing_char.py
+++ missing_char.py
@@ -1,6 +1,7 @@
 
 
 
+
 #Given a non-empty string and an int n, return a new string where the char at index n has been removed. 
 #The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
 
@@ -21,7 +22,6 @@ def missing_char_coding_bat_solution(str, n):
 	front = str[:n]
 	back = str[n+1:]
 	return front + back
-
 
 
 
