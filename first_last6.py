d2ef4275b528eca5df15f8e6c77ad6cb294db6b7
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2019-04-03 13:44:54 -0500

    add function using len list

--- first_last6.py
+++ first_last6.py
@@ -18,3 +18,15 @@ def first_last6(nums):
 print(first_last6([1,2,6]))
 print(first_last6([6,1,2,3]))
 print(first_last6([13, 6, 1, 2, 3]))
+
+
+def first_last6_coding_bat_solution(nums):
+
+	if nums[0] == 6 or nums[len(nums) -1] == 6:
+		return True
+	return False
+
+
+print(first_last6_coding_bat_solution([1,2,6]))
+print(first_last6_coding_bat_solution([6,1,2,3]))
+print(first_last6_coding_bat_solution([13, 6, 1, 2, 3]))
